a70a37894bcbf414e1c0bfed7cabef5ceebbb9a9
eswaribala <parameswaribala@gmail.com>
2021-03-17 15:47:49 +0530

    v1

---
+++ uploadgraphql/src/main/java/com/boa/kyc/upload/resolvers/AdharCardResolver.java
@@ -0,0 +1,24 @@
+package com.boa.kyc.upload.resolvers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.boa.kyc.upload.models.AdharCard;
+import com.boa.kyc.upload.services.AdharCardService;
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+@Component
+public class AdharCardResolver implements GraphQLQueryResolver {
+	@Autowired
+	private AdharCardService adharCardService;
+	
+	public List<AdharCard> findAllAdharCards(){
+		return this.adharCardService.getAllAdharCards();
+	}
+	
+
+	public AdharCard findAdharCard(String adharCardNo ){
+		return this.adharCardService.getAdharCardById(adharCardNo);
+	}
+}
